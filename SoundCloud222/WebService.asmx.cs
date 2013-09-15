3651860e4ae56adc8cfd847ea7e1d11cb7018c99
Ali Yesilkanat <aliyesilkanat@gmail.com>
2013-09-15 12:09:34 +0300

    webservice eklendi

---
+++ SoundCloud222/WebService.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoundCloud222
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://aliyesilkanat.somee.com")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public List<string> getLinks()
+        {
+            List <string> l=new List<string>();
+            aliyesilkanatEntities e = new aliyesilkanatEntities();
+            foreach (sp_ParcalariAl_Result res in e.sp_ParcalariAl())
+                l.Add(res.Link);
+            return l;
+        }
+        
+        
+      
+    }
+   
+}
